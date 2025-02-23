29501b63aabf94b3a44ed56fa51312c5138d263a
junaidiqbalturk <junaidiqbalturk@gmail.com>
2025-02-24 00:30:36 +0500

    Feat: Vue Login Page

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 from flask_cors import CORS
+from config import Config
+
+db = SQLAlchemy()
+jwt = JWTManager()
+
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)  # Allow frontend to communicate with backend
+    app.config.from_object(Config)
 
+    db.init_app(app)
+    jwt.init_app(app)
+    CORS(app)
+
+    # Test route
     @app.route('/api/hello', methods=['GET'])
     def hello():
-        return {"message": "Welcome to PeakPOS API!"}
+        return jsonify({"message": "Welcome to PeakPOS API!"})
+
+    from app.routes.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix="/api/auth")
 
     return app
