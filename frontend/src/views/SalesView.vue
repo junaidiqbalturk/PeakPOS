adf5a059700cc06787e178b3c7095556c1558fe9
junaidiqbalturk <junaidiqbalturk@gmail.com>
2025-05-18 12:07:09 +0500

    Fix: Image displaying issue on Sales page resolved

--- frontend/src/views/SalesView.vue
+++ frontend/src/views/SalesView.vue
@@ -3,7 +3,7 @@
     <!-- Top Action Bar -->
     <div class="sales-action-bar">
       <div class="page-title">
-        <img style="height: 65px;" src="../assets/logo-pp.png"><!--<h2><i class="material-icons">point_of_sale</i> Sales</h2>-->
+        <img style="height: 65px; width: 100px;" src="../assets/logo-pp.png"><!--<h2><i class="material-icons">point_of_sale</i> Sales</h2>-->
         <p class="subtitle">Manage transactions and checkout</p>
       </div>
 
